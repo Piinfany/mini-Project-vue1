efb8c21f7f67744098ebca19c3998555ce77b31c
jirada_dechintaranarak <jirad.de@princhealth.com>
2025-06-23 23:09:33 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -126,7 +126,7 @@ export default {
       return this.salary * 12;
     },
     getDepartment() {
-      return this.salary >= 35000 ? "Project manager" : "Programmer";
+      return this.salary >= 35000 ? "Project manager" : "Developer";
     }   
   },
   watch: {
@@ -143,12 +143,12 @@ export default {
 </script>
 
 <style> 
-/* #app { 
+#app { 
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} */
+}
 </style>
